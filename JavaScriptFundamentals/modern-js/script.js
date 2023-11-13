d54e9a4f172ae25118fb55c9deb83fd94ada570b
thejunghare <prasadjunghare2@gmail.com>
2023-11-13 17:43:40 +0530

    add script

--- JavaScriptFundamentals/modern-js/script.js
+++ JavaScriptFundamentals/modern-js/script.js
@@ -1,1 +1,13 @@
-'use strict'
+'use strict' // Ensure that “use strict” is at the top
+
+/*
+This was the case until 2009 when ECMAScript 5 (ES5) appeared. It added new features to the language and modified some of the existing ones. To keep the old code working, most such modifications are off by default. You need to explicitly enable them with a special directive: "use strict".
+*/
+
+// this code works the modern way
+console.log('I am work the modern way...');
+/* 
+Modern JavaScript supports “classes” and “modules” – advanced language structures (we’ll surely get to them), that enable use strict automatically. So we don’t need to add the "use strict" directive, if we use them.
+
+So, for now "use strict"; is a welcome guest at the top of your scripts. Later, when your code is all in classes and modules, you may omit it.
+*/
