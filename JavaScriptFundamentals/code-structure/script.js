20eea776e1f7c8b1c95bf2dd253b31b7a799e966
thejunghare <prasadjunghare2@gmail.com>
2023-11-13 17:33:57 +0530

    first main

---
+++ JavaScriptFundamentals/code-structure/script.js
@@ -0,0 +1,15 @@
+alert(1);alert(2)
+
+// better way
+alert(3);
+alert(4);
+
+// A semicolon may be omitted in most cases when a line break exists.   
+alert(5)
+alert(6)
+// Here, JavaScript interprets the line break as an “implicit” semicolon. This is called an automatic semicolon insertion.
+
+
+// But this can be problem hence using semicolon are highly appreciated
+alert(7)
+[1, 2].forEach(alert);
