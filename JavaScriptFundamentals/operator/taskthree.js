349e5f39a7ef829aba434fb3d05e205fbc840d73
thejunghare <prasadjunghare2@gmail.com>
2023-11-14 20:59:30 +0530

    add solution

--- JavaScriptFundamentals/operator/taskthree.js
+++ JavaScriptFundamentals/operator/taskthree.js
@@ -3,19 +3,19 @@ Type conversions
 importance: 5
 What are results of these expressions?
 
-"" + 1 + 0
-"" - 1 + 0
-true + false
-6 / "3"
-"2" * "3"
-4 + 5 + "px"
-"$" + 4 + 5
-"4" - 2
-"4px" - 2
-"  -9  " + 5
-"  -9  " - 5
-null + 1
-undefined + 1
-" \t \n" - 2
+"" + 1 + 0 // 10
+"" - 1 + 0 // -1
+true + false // 1
+6 / "3" // 2
+"2" * "3"  // 6
+4 + 5 + "px" // 9px
+"$" + 4 + 5 // $45
+"4" - 2 // 2
+"4px" - 2 // Nan
+"  -9  " + 5 // -9 5
+"  -9  " - 5 // -9 + 5 = -14
+null + 1 // 1 
+undefined + 1  // NaN
+" \t \n" - 2 // -2
 Think well, write down and then compare with the answer.
 */
