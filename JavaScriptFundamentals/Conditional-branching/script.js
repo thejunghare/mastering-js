a7691629664c6fb87aff323ec219c42e3e98dcd7
thejunghare <prasadjunghare2@gmail.com>
2023-11-15 22:50:17 +0530

    add if

---
+++ JavaScriptFundamentals/Conditional-branching/script.js
@@ -0,0 +1,45 @@
+'use strict'
+
+/* 
+if statement evalutes the experssion in the () to boolean
+*/
+
+if (0) {
+    // 0 is  falsy
+    // this never execute
+}
+
+if (1) {
+    // 1 is true
+    // this will execute
+}
+
+let age = 23
+if (age > 18) console.log('true') // short hand
+
+if (age > 18) {
+    console.log('Age is greater than 18')
+    console.log('true');
+}
+
+/* 
+The if statement may contain an optional else block. It executes when the condition is falsy.
+*/
+
+if (0) {
+    // falsy
+} else {
+    console.log('I was executed');
+}
+
+/* 
+Sometimes, we’d like to test several variants of a condition. The else if clause lets us do that.
+*/
+
+if (0) {
+    // falsy
+} else if(1) {
+    console.log('I was executed');
+} else {
+    // falsy
+}
