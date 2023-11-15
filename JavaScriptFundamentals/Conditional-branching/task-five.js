6ca20e2f4bc544a3b86281b191a01bd125a00fd0
thejunghare <prasadjunghare2@gmail.com>
2023-11-15 23:17:42 +0530

    add task five

---
+++ JavaScriptFundamentals/Conditional-branching/task-five.js
@@ -0,0 +1,33 @@
+/* 
+
+Rewrite 'if..else' into '?'
+importance: 5
+Rewrite if..else using multiple ternary operators '?'.
+
+For readability, it’s recommended to split the code into multiple lines.
+
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+*/
+
+
+(login == 'Employee') ? message = 'Hello' :
+    (login == 'Director') ? message = 'Greetings' :
+        (login == '') ? message = 'No login' :
+            message = '';
+
+// OR
+
+let message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
