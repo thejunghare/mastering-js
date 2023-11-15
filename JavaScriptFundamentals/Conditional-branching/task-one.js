338c4398412b2648ba537c30a3f6f83054d1d5a6
thejunghare <prasadjunghare2@gmail.com>
2023-11-15 23:10:30 +0530

    add task 1 and 3

---
+++ JavaScriptFundamentals/Conditional-branching/task-one.js
@@ -0,0 +1,9 @@
+/* 
+if (a string with zero)
+importance: 5
+Will alert be shown?
+
+if ("0") {
+  alert( 'Hello' ); // Yes
+}
+*/
---
+++ JavaScriptFundamentals/Conditional-branching/task-three.js
@@ -0,0 +1,18 @@
+/* 
+Show the sign
+importance: 2
+Using if..else, write the code which gets a number via prompt and then shows in alert:
+
+1, if the value is greater than zero,
+-1, if less than zero,
+0, if equals zero.
+In this task we assume that the input is always a number.
+*/
+
+if (value > 0) {
+    console.log(1);
+} else if (value < 0) {
+    console.log(-1);
+} else {
+    console.log(0);
+}
