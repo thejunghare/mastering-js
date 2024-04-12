318042ab139c139762135236ea9df342c3a41553
thejunghare <prasadjunghare2@gmail.com>
2024-04-12 13:06:05 +0530

    - started 50 days of js - add day 1

---
+++ 50DaysOfJs/One/script.js
@@ -0,0 +1,27 @@
+// Problem statement 👉 https://codedamn.com/problem/_x-IJ93577wKir8ac9t8E?challengeList=50-days-of-js
+
+"use strict"
+
+/* const curry = (f) => {
+  return function (a) {
+    return function (b) {
+      return function (c) {
+        return f(a, b, c)
+      }
+    }
+  }
+} */
+
+const curry = (f) => (a) => (b) => (c) => f(a, b, c)
+
+const sum = (a, b, c) => {
+  return a + b + c
+}
+
+let curriedSum = curry(sum)
+// let curriedSumUsingLodashLibrary = _.curry(sum)
+
+console.log(`call from normal function ${sum(1, 2, 3)}`)
+
+console.log(`call from curry function ${curriedSum(1)(2)(3)}`)
+// console.log(`call from curry function ${curriedSumUsingLodashLibrary(1, 2, 3)}`)
