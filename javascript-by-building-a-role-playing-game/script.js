73610fc33bae04529748c0132ba6680e6cfc40f4
thejunghare <prasadjunghare2@gmail.com>
2024-04-13 15:09:58 +0530

    - added all fight functions , attack, dodge, lose, defeatMonster and restart function

--- javascript-by-building-a-role-playing-game/script.js
+++ javascript-by-building-a-role-playing-game/script.js
@@ -38,27 +38,59 @@ const weapons = [
     },
 ];
 
+const monsters = [
+    {
+        name: 'slime',
+        level: 2,
+        health: 15,
+    },
+    {
+        name: 'fanged beast',
+        level: 8,
+        health: 60,
+    },
+    {
+        name: 'dragon',
+        level: 20,
+        health: 300,
+    },
+];
+
 const locations = [
     {
         name: 'town square',
         'button text': ['Go to store', 'Go to cave', 'Fight dragon'],
         'button functions': [goStore, goCave, fightDragon],
         text: 'You are in the town square. You see a sign that says \"Store"\.'
-    }, {
+    },
+    {
         name: 'store',
         'button text': ['Buy 10 health (10 gold)', 'Buy weapon (30 gold)', 'Go to town square'],
         'button functions': [buyHealth, buyWeapon, goTown],
         text: 'You enter the store.'
-    }
-    , {
+    },
+    {
         name: 'cave',
         'button text': ['Fight slime', 'Fight fanged beast', 'Go to town square'],
         'button functions': [fightSlime, fightBeast, goTown],
         text: 'You enter the cave. You see some monsters.'
+    },
+    {
+        name: 'fight',
+        'button text': ['Attack', 'Dodge', 'Run'],
+        'button functions': [attack, dodge, goTown],
+        text: 'You are fighting a monster.'
+    },
+    {
+        name: 'kill monster',
+        'button text': ['Go to town sqaure', 'Go to town sqaure', 'Go to town sqaure'],
+        'button functions': [goTown, goTown, goTown],
+        text: 'The monster screams "Arg"! as it dies. You gain experience points and find gold.'
     }
 ];
 
 const update = (location) => {
+    monsterStats.style.display = 'none';
     button1.innerText = location['button text'][0];
     button2.innerText = location['button text'][1];
     button3.innerText = location['button text'][2];
@@ -71,7 +103,6 @@ const update = (location) => {
 }
 
 const goTown = () => {
-    // console.log("Going to town.");
     update(locations[0]);
 }
 
@@ -91,9 +122,6 @@ const goStore = () => {
 
 const goCave = () => {
     update(locations[2]);
-}
-
-const fightDragon = () => {
 }
 
 const buyHealth = () => {
@@ -140,8 +168,74 @@ const sellWeapon = () => {
     }
 }
 
-const fightSlime = () => { }
-const fightBeast = () => { }
+const goFight = () => {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = 'block';
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsters[fighting].health;
+}
+
+const fightSlime = () => {
+    fighting = 0;
+    goFight();
+}
+
+const fightBeast = () => {
+    fighting = 1;
+    goFight();
+}
+
+const fightDragon = () => {
+    fighting = 2;
+    goFight();
+}
+
+const attack = () => {
+    text.innerText = `The ${monsters[fighting].name} attacks.`;
+    text.innerText += ` You attack it with your ${weapons[currentWeapon].name}.`;
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp);
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+
+    if (health <= 0) {
+        lose();
+    } else if (monsterHealth <= 0) {
+        defeatMonster();
+    }
+}
+
+const dodge = () => {
+    text.innerText = `You dodge the attack from the ${monsters[fighting].name}.`;
+}
+
+const lose = () => {
+    update(locations[5]);
+}
+
+const defeatMonster = () => {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+}
+
+const restart = () => {
+    xp = 0;
+    health = 100;
+    gold = 50;
+
+    currentWeapon = 0;
+    inventory = ['stick']
+
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+
+    goTown();
+}
 
 button1.onclick = goStore;
 button2.onclick = goCave;
