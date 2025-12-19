016b1d0ae7c8ba2b6f4a4643ecab43e0038b4d6d
thejunghare <prasadjunghare2@gmail.com>
2025-12-19 23:23:57 +0530

    solved problem 2

---
+++ 50DaysOfJsNew/dayTwo.js
@@ -0,0 +1,17 @@
+/* 
+
+Create a JavaScript object named `product` with properties `id`, `name`, `price`, and `availability`
+The `id` and `price` properties of the `product` object should be numbers
+The `name` property of the `product` object should be a non-empty string
+The `availability` property of the `product` object should be a boolean
+
+*/
+
+export let product = {
+	id: 1,
+	name: 'iPhone',
+	price: 100,
+	availability: true
+}
+
+console.log(product)
